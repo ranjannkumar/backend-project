cef50e8f1585469f1e45cc3bd80cef854245efae
Ranjan kumar <ranjankumar5673@gmail.com>
2024-07-04 10:50:57 +0530

    all register controller bug fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -18,8 +18,8 @@ const registerUser=asyncHandler(async(req,res)=>{
 
 
   const {fullName,email,username,password}=req.body
-  console.log("email: ",email);
-  console.log("user data taken: ",req.body);
+ // console.log("email: ",email);
+  //console.log("user data taken: ",req.body);
 
 
   if(
@@ -30,20 +30,25 @@ const registerUser=asyncHandler(async(req,res)=>{
   }
 
 
-  const existedUser=User.findOne({
+  const existedUser=await User.findOne({
     $or: [{ username },{ email }]
   })
-  console.log("checking existing user");
+  console.log("checking existing user :",existedUser);
   if(existedUser){
     throw new ApiError(409,"user with email or username already exists")
   }
 
-
+  // console.log(req.files);
 
   const avatarLocalPath=req.files?.avatar[0]?.path;
-  console.log("avatarlocalpath: ",avatarLocalPath);
-  const coverImageLocalPath=req.files?.coverImage[0]?.path;
-  console.log("coverImagepath: ",coverImageLocalPath);
+  //console.log("avatarlocalpath: ",avatarLocalPath);
+  // const coverImageLocalPath=req.files?.coverImage[0]?.path;
+  let coverImageLocalPath;
+  if (req.files && Array.isArray(req.files.coverImage) 
+  && req.files.coverImage.length>0){
+    coverImageLocalPath=req.files.coverImage[0].path;
+   }
+  //console.log("coverImagepath: ",coverImageLocalPath);
   if(!avatarLocalPath){
     throw new ApiError(400,"Avatar file is required")
   }
@@ -65,7 +70,7 @@ const registerUser=asyncHandler(async(req,res)=>{
     password,
     username: username.toLowerCase()
   })
-  console.log("user created details: ",user);
+ // console.log("user created details: ",user);
 
 
 
